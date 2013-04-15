92c50cd56cd6b335e61d249ec8797c8ab8e5a782
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-15 20:12:39 +0100

    refactor apply discount fn

--- shop_functions.py
+++ shop_functions.py
@@ -5,18 +5,28 @@ PRICES = {
 }
 total = 0
 no_cherries = 0
+
 def is_even(n):
     return n % 2 == 0
+
+
+def apply_discount(s):
+    '''
+    for every second bag of cherries, give a special 20p discount
+    '''
+    global no_cherries
+    if s == 'cherries':
+        no_cherries += 1
+        if is_even(no_cherries):
+            return -20
+    return 0
+
 
 def running_total(s):
     global total, no_cherries
     if s in PRICES:
         total += PRICES[s]
-        if s == 'cherries':
-            no_cherries +=1
-            if is_even(no_cherries):
-                total -= 20
-
+        total += apply_discount(s)
     return '%s : %s' % (s, total)
 
 def resettotal():

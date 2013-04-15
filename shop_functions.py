4db8b01a166baea7bf3ca7cc9b00d771fb925521
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-15 20:06:57 +0100

    cherries get a special 20p discount

--- shop_functions.py
+++ shop_functions.py
@@ -4,10 +4,19 @@ PRICES = {
     'cherries':75
 }
 total = 0
+no_cherries = 0
+def is_even(n):
+    return n % 2 == 0
+
 def running_total(s):
-    global total
+    global total, no_cherries
     if s in PRICES:
         total += PRICES[s]
+        if s == 'cherries':
+            no_cherries +=1
+            if is_even(no_cherries):
+                total -= 20
+
     return '%s : %s' % (s, total)
 
 def resettotal():
--- tests.py
+++ tests.py
@@ -6,13 +6,23 @@ class test_shop(unittest.TestCase):
         resettotal()
 
     def test_buy_one_apple(self):
-        self.assertEqual(running_total('apples'),'apples : 100')
+        self.assertEqual('apples : 100', running_total('apples'))
 
     def test_buy_two_apple(self):
-        self.assertEqual(running_total('apples'),'apples : 100')
-        self.assertEqual(running_total('apples'),'apples : 200')
+        self.assertEqual('apples : 100', running_total('apples'))
+        self.assertEqual('apples : 200', running_total('apples'))
 
     def test_acceptance(self):
-        self.assertEqual(running_total('apples'),'apples : 100')
-        self.assertEqual(running_total('cherries'),'cherries : 175')
-        self.assertEqual(running_total('cherries'),'cherries : 250')
+        self.assertEqual('apples : 100',   running_total('apples'))
+        self.assertEqual('cherries : 175', running_total('cherries'))
+        self.assertEqual('cherries : 230', running_total('cherries'))
+
+    def test_acceptance_with_four_cherries(self):
+        self.assertEqual('apples : 100',   running_total('apples'))
+        self.assertEqual('cherries : 175', running_total('cherries'))
+        self.assertEqual('cherries : 230', running_total('cherries'))
+        self.assertEqual('cherries : 305', running_total('cherries'))
+        self.assertEqual('cherries : 360', running_total('cherries'))
+
+    def test_non_inventory_item(self):
+        self.assertEqual('apple : 0', running_total('apple'))

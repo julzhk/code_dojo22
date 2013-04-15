7c82c0d4d54ef66c8de35ffaea87736b554d9911
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-15 20:42:23 +0100

    allow a list input as well as individual fruits

--- shop_functions.py
+++ shop_functions.py
@@ -1,4 +1,4 @@
-BULK_CHERRY_DISCOUNT = 20
+BULK_CHERRY_DISCOUNT = 30
 PRICES = {
     'apples':100,
     'bananas':150,
@@ -6,6 +6,7 @@ PRICES = {
 }
 total = 0
 no_cherries = 0
+no_bananas = 0
 
 def is_even(n):
     return n % 2 == 0
@@ -15,20 +16,37 @@ def apply_discount(s):
     '''
     for every second bag of cherries, give a special 20p discount
     '''
-    global no_cherries
+    global no_cherries, no_bananas
     if s == 'cherries':
         no_cherries += 1
         if is_even(no_cherries):
             return -BULK_CHERRY_DISCOUNT
+    if s == 'bananas':
+        no_bananas  += 1
+        if is_even(no_bananas):
+            return -PRICES['bananas']
+
     return 0
 
 
-def running_total(s):
+def add_to_running_total(bought_item):
+    global total
+    if bought_item in PRICES:
+        total += PRICES[bought_item]
+        total += apply_discount(bought_item)
+    return '%s : %s' % (bought_item, total)
+
+
+def running_total(bought_item):
     global total, no_cherries
-    if s in PRICES:
-        total += PRICES[s]
-        total += apply_discount(s)
-    return '%s : %s' % (s, total)
+    if ',' in bought_item:
+        productlist = bought_item.split(',')
+        for product in productlist:
+            add_to_running_total(product)
+        return '%s : %s' % (bought_item, total)
+
+    else:
+        return add_to_running_total(bought_item)
 
 def resettotal():
     global total
--- tests.py
+++ tests.py
@@ -15,14 +15,31 @@ class test_shop(unittest.TestCase):
     def test_acceptance(self):
         self.assertEqual('apples : 100',   running_total('apples'))
         self.assertEqual('cherries : 175', running_total('cherries'))
-        self.assertEqual('cherries : 230', running_total('cherries'))
+        self.assertEqual('cherries : 220', running_total('cherries'))
 
     def test_acceptance_with_four_cherries(self):
         self.assertEqual('apples : 100',   running_total('apples'))
         self.assertEqual('cherries : 175', running_total('cherries'))
-        self.assertEqual('cherries : 230', running_total('cherries'))
-        self.assertEqual('cherries : 305', running_total('cherries'))
-        self.assertEqual('cherries : 360', running_total('cherries'))
+        self.assertEqual('cherries : 220', running_total('cherries'))
+        self.assertEqual('cherries : 295', running_total('cherries'))
+        self.assertEqual('cherries : 340', running_total('cherries'))
 
     def test_non_inventory_item(self):
         self.assertEqual('apple : 0', running_total('apple'))
+
+    def test_banana_buy_one_get_one_free(self):
+        self.assertEqual(running_total('bananas'),'bananas : 150' )
+        self.assertEqual(running_total('bananas'),'bananas : 150' )
+        self.assertEqual(running_total('bananas'),'bananas : 300' )
+        self.assertEqual(running_total('bananas'),'bananas : 300' )
+
+    def test_csv_input(self):
+        self.assertEqual(running_total('apples,cherries,bananas'),'apples,cherries,bananas : 325')
+        # self.assertEqual(running_total('apple, cherries, bananas'),325)
+        # self.assertEqual(running_total('Apple,Cherries,Bananas'),325)
+        # self.assertEqual(running_total('Apple, Cherries, Bananas'),325)
+        # self.assertEqual(running_total('Cherries,Cherries'), 130)
+        # self.assertEqual(running_total('Cherries, Cherries'), 130)
+        # self.assertEqual(running_total('cherries,cherries'), 130)
+        # self.assertEqual(running_total('cherries, cherries'), 130)
+        pass

c0b633b42122bf1d67b3b36b1174c4d8859e372c
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-16 19:22:39 +0100

    refactor tests, add some comments

--- tests.py
+++ tests.py
@@ -12,12 +12,16 @@ class test_shop(unittest.TestCase):
         self.assertEqual('apples : 100', running_total('apples'))
         self.assertEqual('apples : 200', running_total('apples'))
 
-    def test_acceptance(self):
+    def test_acceptance_test1(self):
         self.assertEqual('apples : 100',   running_total('apples'))
         self.assertEqual('cherries : 175', running_total('cherries'))
         self.assertEqual('cherries : 230', running_total('cherries'))
 
-    def test_acceptance_with_four_cherries(self):
+    def test_acceptance_test_2_with_four_cherries(self):
+        '''
+        new client requiremnt: buy 2 sets of cherries &
+        get a discount of 20p
+        '''
         self.assertEqual('apples : 100',   running_total('apples'))
         self.assertEqual('cherries : 175', running_total('cherries'))
         self.assertEqual('cherries : 230', running_total('cherries'))
@@ -27,13 +31,20 @@ class test_shop(unittest.TestCase):
     def test_non_inventory_item(self):
         self.assertEqual('apple : 0', running_total('apple'))
 
-    def test_banana_buy_one_get_one_free(self):
+    def test_acceptance_test_3_banana_buy_one_get_one_free(self):
+        '''
+        we now have a special offer on bananas,
+        buy one get one free
+        '''
         self.assertEqual(running_total('bananas'),'bananas : 150' )
         self.assertEqual(running_total('bananas'),'bananas : 150' )
         self.assertEqual(running_total('bananas'),'bananas : 300' )
         self.assertEqual(running_total('bananas'),'bananas : 300' )
 
     def test_csv_input(self):
+        '''
+        We want to accept a list of items as well as singularly
+        '''
         self.assertEqual(running_total('apples,cherries,bananas'),'apples,cherries,bananas : 325')
 
     def test_dirty_csv_input(self):
@@ -52,7 +63,7 @@ class test_shop(unittest.TestCase):
     def test_allow_mele(self):
         self.assertEqual(running_total('mele'),'mele : 100')
 
-    def acceptance_test_2(self):
+    def acceptance_test_4(self):
         self.assertEqual(running_total('cherries'),'cherries : 75')
         self.assertEqual(running_total('pommes'),'pommes : 175')
         self.assertEqual(running_total('cherries'),'cherries : 230')
@@ -74,3 +85,10 @@ class test_shop(unittest.TestCase):
         self.assertEqual(running_total('mele'),'mele : 450')
         self.assertEqual(running_total('cherries'),'cherries : 525')
         self.assertEqual(running_total('cherries'),'cherries : 580')
+
+
+    def test_iteration_6_acceptance_test(self):
+        # client requiremnt:
+        # 4 apples ( with any name) get a discount of -100
+        # and 5 item get a discount -200
+        self.assertEqual(running_total('mele,pommes,pommes,mele'),'mele,pommes,pommes,mele : 250')

40d6990f830c4683db8403a20ded15643204c34e
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-16 22:50:10 +0100

    further refactoring tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-from shop_functions import (running_total, resettotal, clean_string, calculate_totals)
+from shop_functions import (running_total, resettotal,
+                            clean_string, calculate_totals, PRICES, BULK_CHERRY_DISCOUNT)
 
 class test_shop(unittest.TestCase):
     def setUp(self):
@@ -9,68 +10,93 @@ class test_shop(unittest.TestCase):
         self.assertEqual('apples : 100', running_total('apples'))
 
     def test_buy_one_apple_total(self):
-        self.assertEqual(100, calculate_totals('apples'))
+        self.assertEqual(PRICES['apples'], calculate_totals('apples'))
 
     def test_buy_two_apple(self):
-        self.assertEqual('apples : 100', running_total('apples'))
-        self.assertEqual('apples : 200', running_total('apples'))
+        self.assertEqual(PRICES['apples'], calculate_totals('apples'))
+        self.assertEqual(2 * PRICES['apples'], calculate_totals('apples'))
 
     def test_acceptance_test1(self):
-        self.assertEqual('apples : 100',   running_total('apples'))
-        self.assertEqual('cherries : 175', running_total('cherries'))
-        self.assertEqual('cherries : 230', running_total('cherries'))
+        self.assertEqual(PRICES['apples'], calculate_totals('apples'))
+        self.assertEqual(PRICES['apples']+
+                         PRICES['cherries'], calculate_totals('cherries'))
+
+        self.assertEqual(
+                         PRICES['apples']+
+                         PRICES['cherries']+
+                         PRICES['cherries']-
+                         BULK_CHERRY_DISCOUNT, calculate_totals('cherries'))
 
     def test_acceptance_test_2_with_four_cherries(self):
         '''
         new client requiremnt: buy 2 sets of cherries &
         get a discount of 20p
         '''
-        self.assertEqual('apples : 100',   running_total('apples'))
-        self.assertEqual('cherries : 175', running_total('cherries'))
-        self.assertEqual('cherries : 230', running_total('cherries'))
-        self.assertEqual('cherries : 305', running_total('cherries'))
-        self.assertEqual('cherries : 360', running_total('cherries'))
+        self.assertEqual(PRICES['apples'],   calculate_totals('apples'))
+        self.assertEqual(PRICES['apples']+PRICES['cherries'], calculate_totals('cherries'))
+        self.assertEqual(
+            PRICES['apples']+
+            PRICES['cherries']+
+            PRICES['cherries']-
+            BULK_CHERRY_DISCOUNT, calculate_totals('cherries'))
+        self.assertEqual(
+            PRICES['apples'] +
+            PRICES['cherries'] +
+            PRICES['cherries'] +
+            PRICES['cherries'] -
+            BULK_CHERRY_DISCOUNT, calculate_totals('cherries'))
+        self.assertEqual(
+            PRICES['apples'] +
+            PRICES['cherries'] +
+            PRICES['cherries'] +
+            -BULK_CHERRY_DISCOUNT+
+            PRICES['cherries'] +
+            PRICES['cherries'] -
+            BULK_CHERRY_DISCOUNT
+
+            , calculate_totals('cherries'))
 
     def test_non_inventory_item(self):
-        self.assertEqual('apple : 0', running_total('apple'))
+        self.assertEqual('pineapple : 0', running_total('pineapple'))
 
     def test_acceptance_test_3_banana_buy_one_get_one_free(self):
         '''
         we now have a special offer on bananas,
         buy one get one free
         '''
-        self.assertEqual(running_total('bananas'),'bananas : 150' )
-        self.assertEqual(running_total('bananas'),'bananas : 150' )
-        self.assertEqual(running_total('bananas'),'bananas : 300' )
-        self.assertEqual(running_total('bananas'),'bananas : 300' )
+        self.assertEqual(calculate_totals('bananas'),PRICES['bananas'] )
+        self.assertEqual(calculate_totals('bananas'),PRICES['bananas'] )
+        self.assertEqual(calculate_totals('bananas'),PRICES['bananas']+PRICES['bananas'] )
+        self.assertEqual(calculate_totals('bananas'),PRICES['bananas']+PRICES['bananas'] )
 
     def test_csv_input(self):
         '''
         We want to accept a list of items as well as singularly
         '''
-        self.assertEqual(running_total('apples,cherries,bananas'),'apples,cherries,bananas : 325')
+        self.assertEqual(calculate_totals('apples,cherries,bananas'),
+                         PRICES['apples']+PRICES['cherries']+PRICES['bananas'])
 
     def test_dirty_csv_input1(self):
-        self.assertEqual(running_total('apples, cherries, bananas'),'apples, cherries, bananas : 325')
+        self.assertEqual(calculate_totals('apples, cherries, bananas'),
+                         PRICES['apples']+PRICES['cherries']+PRICES['bananas'])
     def test_dirty_csv_input2(self):
-        self.assertEqual(running_total('Apples,Cherries,Bananas'),'Apples,Cherries,Bananas : 325')
+        self.assertEqual(calculate_totals('Apples,Cherries,Bananas'),
+                         PRICES['apples']+PRICES['cherries']+PRICES['bananas'])
     def test_dirty_csv_input3(self):
-        self.assertEqual(running_total('Apples, Cherries, Bananas'),'Apples, Cherries, Bananas : 325')
+        self.assertEqual(calculate_totals('Apples, Cherries, Bananas'),
+                         PRICES['apples']+PRICES['cherries']+PRICES['bananas'])
     def test_dirty_csv_input4(self):
-        self.assertEqual(running_total('Cherries,Cherries'), 'Cherries,Cherries : 130')
+        self.assertEqual(calculate_totals('Cherries,Cherries'),
+                         PRICES['cherries']+PRICES['cherries']-BULK_CHERRY_DISCOUNT)
     def test_dirty_csv_input5(self):
-        self.assertEqual(running_total('Cherries, Cherries'), 'Cherries, Cherries : 130')
-    def test_dirty_csv_input6(self):
-        self.assertEqual(running_total('cherries,cherries'), 'cherries,cherries : 130')
-    def test_dirty_csv_input7(self):
-        self.assertEqual(running_total('cherries, cherries'), 'cherries, cherries : 130')
-        pass
+        self.assertEqual(calculate_totals('Cherries, Cherries'),
+                         PRICES['cherries']+PRICES['cherries']-BULK_CHERRY_DISCOUNT)
 
     def test_allow_pommes(self):
-        self.assertEqual(running_total('pommes'),'pommes : 100')
+        self.assertEqual(calculate_totals('pommes'),PRICES['pommes'])
 
     def test_allow_mele(self):
-        self.assertEqual(running_total('mele'),'mele : 100')
+        self.assertEqual(calculate_totals('mele'),PRICES['mele'])
 
     def acceptance_test_4(self):
         self.assertEqual(running_total('cherries'),'cherries : 75')
